209efc6e3d8e01d409ffcb813ccb180b05bca1a5
Lokksli <mgrapinuk@gmail.com>
2026-05-01 23:11:53 +0200

    Add nrf24 radio module

--- potatao_phone/build_micropython.sh
+++ potatao_phone/build_micropython.sh
@@ -13,9 +13,9 @@ cd micropython/ports/rp2
 rm -rf build
 
 cmake -S . -B build \
-    -DPICO_BOARD=pico2_w \
-    -DBOARD=RPI_PICO2_W \
-    -DMICROPY_BOARD=RPI_PICO2_W \
+    -DPICO_BOARD=pico_w\
+    -DBOARD=RPI_PICO_W \
+    -DMICROPY_BOARD=RPI_PICO_W \
     -DPICO_SDK_PATH="$SDK_PATH" \
     -DUSER_C_MODULES="$MODULE_PATHS"
 
